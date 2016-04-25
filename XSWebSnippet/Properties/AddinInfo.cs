5bc937b82481580cd155b17bf7127ce610365f86
Martynnw <Martyn@Martyns-MacBook-Pro.local>
2016-04-25 20:48:17 +0100

    Initial commit

---
+++ XSWebSnippet/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+	"XSWebSnippet",
+	Namespace = "XSWebSnippet",
+	Version = "1.0"
+)]
+
+[assembly: AddinName("XSWebSnippet")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("XSWebSnippet")]
+[assembly: AddinAuthor("Martyn")]
+
